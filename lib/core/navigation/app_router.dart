a249d899a3fb47b5cc2016848de00c29cf5dd239
Goodnews Azonubi <azonubigoodnews@gmail.com>
2025-11-14 11:03:11 +0100

    AppRouter(update): added router for forgot password and change password and change password success screen

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -1,8 +1,11 @@
+import 'package:dive_fintech/features/auth/presentation/pages/change_password_page.dart';
+import 'package:dive_fintech/features/auth/presentation/pages/change_password_success_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/signup_page.dart';
 import '../../features/auth/presentation/pages/forgot_password_page.dart';
+import '../../features/auth/presentation/pages/forgot_password_screen.dart';
 import '../../features/home/presentation/pages/home_page.dart';
 import '../../features/home/presentation/pages/main_navigation_page.dart';
 
@@ -11,7 +14,10 @@ class AppRoutes {
   static const String login = '/login';
   static const String signup = '/signup';
   static const String forgotPassword = '/forgot-password';
-  
+  static const String forgotPasswordCode = '/forgot-password-code';
+  static const String changePassword = '/change-password';
+  static const String changePasswordSuccess = '/change-password-success';
+
   // Main routes
   static const String home = '/';
   static const String search = '/search';
@@ -39,7 +45,22 @@ class AppRouter {
         name: 'forgot-password',
         builder: (context, state) => const ForgotPasswordPage(),
       ),
-      
+      GoRoute(
+        path: AppRoutes.forgotPasswordCode,
+        name: 'forgot-password-code',
+        builder: (context, state) => const ForgotPasswordPageCode(),
+      ),
+      GoRoute(
+        path: AppRoutes.changePassword,
+        name: 'change-password',
+        builder: (context, state) => const ChangePasswordPage(),
+      ),
+      GoRoute(
+        path: AppRoutes.changePasswordSuccess,
+        name: 'change-password-success',
+        builder: (context, state) => const ChangePasswordSuccessPage(),
+      ),
+
       // Main app routes with bottom navigation
       ShellRoute(
         builder: (context, state, child) {
